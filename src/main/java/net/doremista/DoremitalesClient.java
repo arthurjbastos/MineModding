c8a1b8c548fef502ea9d04d13b8d2742aedeb0c4
Arthur Bastos <arthurj.bastos4@gmail.com>
2024-08-06 17:20:15 -0300

    Initial commit

---
+++ src/main/java/net/doremista/DoremitalesClient.java
@@ -0,0 +1,10 @@
+package net.doremista;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoremitalesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/doremista/DoremitalesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.doremista;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DoremitalesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
